7c6b885b469f282212f37c6cce988584b963a451
SeHoon <sehoon1989@gmail.com>
2024-10-04 23:42:49 +0900

    feature/add-sample-extra-lib-load

--- app/sample/iframe-parent/page.tsx
+++ app/sample/iframe-parent/page.tsx
@@ -31,6 +31,26 @@ const iframeParent = () => {
     serCounter(counter + 1);
     iframeRef.current?.contentWindow?.postMessage(`Hello from parent ${counter}`, 'http://localhost:3001'); // Replace with your iframe's origin
   };
+
+  return (
+    <div>
+      <h1>Iframe Parent Component</h1>
+      <button onClick={sendMessageToIframe} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Send Message to Iframe
+      </button>
+      <p>Message from iframe: {message}</p>
+      <div className="border-2 border-black p-2 inline-block">
+        <iframe
+          ref={iframeRef}
+          src="http://localhost:3001/sample/iframe-child" // Replace with your iframe's URL
+          width="600"
+          height="400"
+          title="Iframe Example"
+          className="border-2 border-gray-300"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default iframeParent;
