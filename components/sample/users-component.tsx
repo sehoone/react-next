1739b71582ef8404a65ec2ec97ac4a1c63a3b999
SeHoon <sehoon1989@gmail.com>
2024-10-11 00:33:24 +0900

    feature/add-multi-react-query

--- components/sample/users-component.tsx
+++ components/sample/users-component.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useUsers } from '@/service/sample/sample-query';
+import { useMovies } from '@/hooks/useMovieApi';
 
 const EffectComponent = () => {
-  const { data, error, isLoading, refetch } = useUsers();
+  const { data: users, error: usersError, isLoading: usersLoading, refetch: usersRefatch } = useUsers();
+  const { data: movies, error: moviesError, isLoading: moviesLoading, refetch: moviesRefetch } = useMovies({ queryKey: ['movies'], enabled: false }); // enabled 초기 진입시 호출하지 않음
   const [counter, serCounter] = useState<number>(0);
 
   // useEffect 훅을 사용해서 처음 렌더링 될 때만 실행되도록 처리
@@ -17,15 +19,20 @@ const EffectComponent = () => {
 
   return <>
 
-    <button onClick={() => refetch} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">Refresh</button>
+    <button onClick={() => usersRefatch()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+      Refresh Users
+    </button>
+    <button onClick={() => moviesRefetch()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+      Refresh Movies
+    </button>
     <button onClick={incrementCounter} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">Counter {counter}</button>
     <Link href="/sample/react-query"
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
     >
       go another page
     </Link>
-    <div>{data ? (
-      data.map(user => (
+    <div>{users ? (
+      users.map(user => (
         <div key={user.name} className="bg-white shadow-md rounded-lg overflow-hidden">
           <div className="p-4">
             <h2 className="text-xl font-bold mb-2">{user.name}</h2>
@@ -37,6 +44,23 @@ const EffectComponent = () => {
       <p className="text-center text-gray-500">Loading...</p>
     )}
 
+    </div>
+
+    <div>
+      {moviesLoading ? (
+        <p className="text-center text-gray-500">Loading movies...</p>
+      ) : moviesError ? (
+        <p className="text-center text-red-500">Error loading movies: {moviesError.message}</p>
+      ) : (
+        movies?.map((movie: any) => (
+          <div key={movie.title} className="bg-white shadow-md rounded-lg overflow-hidden">
+            <div className="p-4">
+              <h2 className="text-xl font-bold mb-2">{movie.title}</h2>
+              <p className="text-gray-700 mb-2">Rating: {movie.rating}</p>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   </>;
 };
--- hooks/useMovieApi.ts
+++ hooks/useMovieApi.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { defHttp } from '@/utils/http/axios';
 
 enum Api {
@@ -19,10 +19,11 @@ const httpMovies = async (): Promise<Movie[]> => {
   });
   return data;
 }
-export const useMovies = () => {
+export const useMovies = (options?: UseQueryOptions<Movie[], Error>) => {
   return useQuery<Movie[], Error>({
     queryKey: ['movies'],
     queryFn: httpMovies,
     staleTime: 1000 * 60 * 1, // 1분 동안 캐시된 데이터를 사용
+    ...options, // 전달된 옵션을 병합합니다.
   });
 };
