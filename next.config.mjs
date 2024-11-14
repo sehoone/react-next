d406f03c15e2c4ce40991341be825b6804117f01
SeHoon <sehoon1989@gmail.com>
2024-11-14 21:31:15 +0900

    feature/add-source-map-config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
   transpilePackages: ['jotai-devtools'],
   output: 'export', // 빌드된 파일을 /out 디렉토리에 넣기 위한 설정
   basePath: process.env.BASE_PATH || '',
+  productionBrowserSourceMaps: process.env.BROUSER_SOURCE_MAPS, // 브라우저 소스맵 설정. 기본은 false. 빌드된 코드와 원본 소스 코드 간의 매핑 정보를 제공하는 파일
 };
 
 export default nextConfig;
